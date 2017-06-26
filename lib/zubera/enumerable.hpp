669783f0e855d9e9f3d04eb4f8801f0b01adfd13
manga_osyo <manga.osyo@gmail.com>
2017-06-27 04:02:50 +0900

    Sorted define functions.

--- lib/zubera/enumerable.hpp
+++ lib/zubera/enumerable.hpp
@@ -20,10 +20,26 @@ struct enumerable{
 		return *static_cast<self_t const*>(this);
 	}
 
-	constexpr bool
-	is_empty() const{
-		return count() == 0;
+
+	template<typename Pred>
+	constexpr std::size_t
+	count_if(Pred&& pred) const{
+		return self().inject(0, [&pred](auto sum, auto it) constexpr {
+			return pred(it) ? sum + 1 : sum;
+		});
 	}
+
+	constexpr auto
+	count() const{
+		return self().count_if([](auto...) constexpr { return true; });
+	}
+
+	template<typename T>
+	constexpr auto
+	count(T&& t) const{
+		return self().count_if([&t](auto it){ return it == t; });
+	}
+
 
 	template<typename F>
 	constexpr auto
@@ -32,6 +48,38 @@ struct enumerable{
 			f(it, i);
 			return ++i;
 		});
+	}
+
+	template<typename Init, typename F>
+	constexpr auto
+	inject(Init&& init, F f) const{
+		self().each([&](auto n) constexpr {
+			init = f(std::forward<Init>(init), n);
+		});
+		return init;
+	}
+
+	template<typename F>
+	auto
+	map(F f){
+		using result_t = Result<decltype(f(std::declval<Value>()))>;
+		return self().inject(result_t{}, [&f](auto result, auto it){
+			return result.push(f(it));
+		});
+	}
+
+	template<typename Pred>
+	auto
+	select(Pred pred){
+		return self().inject(Tarray_t{}, [&pred](auto sum, auto it){
+			return pred(it) ? sum.push(it) : sum;
+		});
+	}
+
+
+	constexpr bool
+	is_empty() const{
+		return count() == 0;
 	}
 
 	template<typename T, typename F>
@@ -67,52 +115,6 @@ struct enumerable{
 	constexpr bool
 	operator !=(T&& t) const{
 		return !self().equal(std::forward<T>(t));
-	}
-
-	template<typename Init, typename F>
-	constexpr auto
-	inject(Init&& init, F f) const{
-		self().each([&](auto n) constexpr {
-			init = f(std::forward<Init>(init), n);
-		});
-		return init;
-	}
-
-
-	template<typename Pred>
-	constexpr std::size_t
-	count_if(Pred&& pred) const{
-		return self().inject(0, [&pred](auto sum, auto it) constexpr {
-			return pred(it) ? sum + 1 : sum;
-		});
-	}
-
-	constexpr auto
-	count() const{
-		return self().count_if([](auto...) constexpr { return true; });
-	}
-
-	template<typename T>
-	constexpr auto
-	count(T&& t) const{
-		return self().count_if([&t](auto it){ return it == t; });
-	}
-
-	template<typename Pred>
-	auto
-	select(Pred pred){
-		return self().inject(Tarray_t{}, [&pred](auto sum, auto it){
-			return pred(it) ? sum.push(it) : sum;
-		});
-	}
-
-	template<typename F>
-	auto
-	map(F f){
-		using result_t = Result<decltype(f(std::declval<Value>()))>;
-		return self().inject(result_t{}, [&f](auto result, auto it){
-			return result.push(f(it));
-		});
 	}
 };
 
