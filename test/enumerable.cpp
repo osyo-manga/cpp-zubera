d614e56d74aab4fbd75171e4bab5f721ba2110dd
manga_osyo <manga.osyo@gmail.com>
2017-06-30 13:55:31 +0900

    Change REQUIRE to CHECK.

--- test/enumerable.cpp
+++ test/enumerable.cpp
@@ -9,19 +9,19 @@ test_enumerable_functions(Maker make){
 	using namespace test;
 
 	SECTION("all_of"){
-		REQUIRE(make().all_of(is_under(3)));
-		REQUIRE(make(1, 2, 3).all_of(is_under(3)));
-		REQUIRE(!make(1, 2, 3, 4).all_of(is_under(3)));
+		CHECK(make().all_of(is_under(3)));
+		CHECK(make(1, 2, 3).all_of(is_under(3)));
+		CHECK(!make(1, 2, 3, 4).all_of(is_under(3)));
 	}
 
 	SECTION("count"){
-		REQUIRE(make().count() == 0);
+		CHECK(make().count() == 0);
 
-		REQUIRE(make(1, 2, 3).count() == 3);
-		REQUIRE(make(1, 2, 3, 4, 5).count_if(is_over(3)) == 3);
-		REQUIRE(make(1, 1, 2, 2, 2, 3).count_if(is_equal(2)) == 3);
-		REQUIRE(make(1, 1, 2, 2, 2, 3).count(2) == 3);
-		REQUIRE(make("homu", "homu", "mami").count(std::string("homu")) == 2);
+		CHECK(make(1, 2, 3).count() == 3);
+		CHECK(make(1, 2, 3, 4, 5).count_if(is_over(3)) == 3);
+		CHECK(make(1, 1, 2, 2, 2, 3).count_if(is_equal(2)) == 3);
+		CHECK(make(1, 1, 2, 2, 2, 3).count(2) == 3);
+		CHECK(make("homu", "homu", "mami").count(std::string("homu")) == 2);
 	}
 
 	SECTION("each"){
@@ -30,7 +30,7 @@ test_enumerable_functions(Maker make){
 		make(1, 2, 3, 4, 5).each([&](auto n){
 			each_result.push_back(n);
 		});
-		REQUIRE((each_result == vec_t{1, 2, 3, 4, 5}));
+		CHECK((each_result == vec_t{1, 2, 3, 4, 5}));
 	}
 
 	SECTION("each_with_index"){
@@ -43,25 +43,25 @@ test_enumerable_functions(Maker make){
 			result_values.push_back(it);
 		});
 
-		REQUIRE((result_indices == vec_t{0, 1, 2, 3, 4}));
-		REQUIRE((result_values  == vec_t{5, 4, 3, 2, 1}));
+		CHECK((result_indices == vec_t{0, 1, 2, 3, 4}));
+		CHECK((result_values  == vec_t{5, 4, 3, 2, 1}));
 	}
 
 	SECTION("inject"){
-		REQUIRE((make(1, 2, 3).inject(0, plus) == 6));
+		CHECK((make(1, 2, 3).inject(0, plus) == 6));
 	}
 
 	SECTION("map"){
 		using namespace std::literals::string_literals;
 		auto to_string = [](auto it){ return std::to_string(it); };
 
-		REQUIRE(make(1, 2, 3).map(twice) == make(2, 4, 6));
-		REQUIRE(make(1, 2, 3).map(to_string) == make("1"s, "2"s, "3"s));
+		CHECK(make(1, 2, 3).map(twice) == make(2, 4, 6));
+		CHECK(make(1, 2, 3).map(to_string) == make("1"s, "2"s, "3"s));
 	}
 
 	SECTION("select"){
-		REQUIRE(make(1, 2, 3, 4, 5, 6).select(is_even) == make(2, 4, 6));
-		REQUIRE(make(1, 2, 3, 4, 5, 6).select(is_odd)  == make(1, 3, 5));
+		CHECK(make(1, 2, 3, 4, 5, 6).select(is_even) == make(2, 4, 6));
+		CHECK(make(1, 2, 3, 4, 5, 6).select(is_odd)  == make(1, 3, 5));
 	}
 }
 
